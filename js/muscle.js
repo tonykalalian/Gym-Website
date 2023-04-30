59a8dd3db3d7c0121216de9faebbec4c5ab98365
Tony Kalalian <tkalalian@gmail.com>
2023-04-30 19:26:25 +0300

    each muscle group redirects to a special exercise

---
+++ js/muscle.js
@@ -0,0 +1,59 @@
+let lats = document.getElementById("Lats").addEventListener("click", () => {
+  window.location.href = "/exercises/lats.html";
+});
+let trapezius = document
+  .getElementById("Trapezius")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/trapezius.html";
+  });
+let triceps = document
+  .getElementById("Triceps")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/triceps.html";
+  });
+let forearms = document
+  .getElementById("Forearms")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/forearms.html";
+  });
+let glutes = document.getElementById("Glutes").addEventListener("click", () => {
+  window.location.href = "/exercises/glutes.html";
+});
+let hamstring = document
+  .getElementById("Hamstrings")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/hamstring.html";
+  });
+let calves = document.getElementById("Calves").addEventListener("click", () => {
+  window.location.href = "/exercises/calves.html";
+});
+let deltoids = document
+  .getElementById("Deltoids")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/deltoids.html";
+  });
+let biceps = document.getElementById("Biceps").addEventListener("click", () => {
+  window.location.href = "/exercises/biceps.html";
+});
+let obliques = document
+  .getElementById("Obliques")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/obliques.html";
+  });
+
+let abs = document.getElementById("Abs").addEventListener("click", () => {
+  window.location.href = "/exercises/abs.html";
+});
+let quads = document.getElementById("Quads").addEventListener("click", () => {
+  window.location.href = "/exercises/quads.html";
+});
+let adductors = document
+  .getElementById("Adductors")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/adductors.html";
+  });
+let pectorals = document
+  .getElementById("Pectorals")
+  .addEventListener("click", () => {
+    window.location.href = "/exercises/chest.html";
+  });
